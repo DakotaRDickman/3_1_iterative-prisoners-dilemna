ea9836e88319ab7d775a040f3ce03c16b46a89df
DakotaRDickman <dakota.dickman@students.sacs.k12.in.us>
2016-12-20 09:40:35 -0500

    Tells where the game starts

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -391,5 +391,5 @@ def post_to_file(string, filename='tournament.txt', directory=''):
 if __name__ == '__main__':
     scores, moves, reports = main_play(modules[0:4])   
     section0, section1, section2, section3 = reports
-    print: 'Beginning Game...'
+    #This function begins the game
    
